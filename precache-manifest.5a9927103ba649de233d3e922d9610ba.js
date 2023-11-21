78b7d649aa35e5cb74519352275925f0450873d1
Emmanuel Akosa <akosaemmanuel45@gmail.com>
2023-11-21 13:20:58 +0000

    Updates

---
+++ precache-manifest.5a9927103ba649de233d3e922d9610ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cc2d8238693fa17a8a32bb00b9d721",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ee21b7919f1f9612764",
+    "url": "/monsters-rolodex/static/css/main.96077864.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/monsters-rolodex/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/monsters-rolodex/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "1ee21b7919f1f9612764",
+    "url": "/monsters-rolodex/static/js/main.24e4e5be.chunk.js"
+  },
+  {
+    "revision": "4032d0a39dcdf2f5a75a",
+    "url": "/monsters-rolodex/static/js/runtime~main.c5797289.js"
+  }
+]);
